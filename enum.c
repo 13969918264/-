28bb7d0fca9eb0ed6b992bcc3b8e1c1f031c3cc3
赵子豪 <zihao13969918264@hotmail.com>
2017-03-08 16:48:29 +0800

    Create enum.c

---
+++ enum.c
@@ -0,0 +1,4 @@
+enum escapes {	BELL = '\a',BACKSPACE ='\b' ,TAB = '\t',
+				NEWLINE = '\n',VTAB = '\v',RETURN = '\r'};
+enum months {	JAN = 1,FRB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+/*	FEB的值是2，以此类推	*/
